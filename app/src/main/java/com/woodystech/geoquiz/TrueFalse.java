d618a9e503bf56b02a5e08246f26e4556ebc305c
Brian Widhalm <bdwidhalm@gmail.com>
2015-10-02 07:38:30 -0600

    initial commit from Android Studio

---
+++ app/src/main/java/com/woodystech/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.woodystech.geoquiz;
+
+/**
+ * Created by brian on 8/24/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
